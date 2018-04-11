89030a75f7492090ee13322e4e4ccc8632f334ae
srydell <simonwrydell@gmail.com>
2018-04-11 16:06:09 +0000

    Moved scripts to ~/bin

---
+++ bin/system/wall_looper.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Feed this script two arguments: $1 for a folder directory and $2 number of seconds per image.
+
+# Will loop through the images, using feh to change the background every $2 seconds.
+
+while [ 1==1 ]
+	do
+		for file in $1*;
+		do
+			sleep $2
+			echo "$file"
+			feh --bg-scale "$file"
+			cp "$file" ~/.config/wallpaper.png
+		done
+	done
