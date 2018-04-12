5dc567560a569e98d59cac53b26d04934bc5d62f
srydell <simonwrydell@gmail.com>
2018-04-12 09:34:21 +0000

    Write transparentBar and greyBar and picked a naming convention for the wallpapers

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,10 +7,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch mainBar on each available monitor
-for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar mainBar -c ~/.config/polybar/config & done
-
-# Set the background
-feh --bg-scale ~/.config/wallpaper.jpg
+# Launch bar $1 on each available monitor
+for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar $1 -c ~/.config/polybar/config & done
 
 echo "Bars launched..."
