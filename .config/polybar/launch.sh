00defd07b526ad72cd4d40ae5c3edfed39ed86cf
srydell <simonwrydell@gmail.com>
2018-05-13 19:34:17 +0200

    Reload polybar config on change with -r flag

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,7 +8,7 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch mainBar on each available monitor
-for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar mainBar -c ~/.config/polybar/config & done
+for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar mainBar -r -c ~/.config/polybar/config & done
 
 # Set the background
 feh --bg-scale ~/.config/wallpaper.jpg
