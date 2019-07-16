ce8efa432f20f714808ceb5b5ae78678659bef2b
srydell <simonwrydell@gmail.com>
2019-07-16 14:33:42 +0200

    Fixed small error in conan setup template

--- vim/.vim/integrations/templates/cmake/SetupConan.cmake
+++ vim/.vim/integrations/templates/cmake/SetupConan.cmake
@@ -1,8 +1,5 @@
 include_guard()
 
-# Require that conan is installed
-conan_check(REQUIRED)
-
 # Downloads module to run conan from cmake
 function(get_conan_helper)
   if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
