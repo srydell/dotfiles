ed09657a2789c81acc7dd0f5e7254efa006d0438
srydell <simonwrydell@gmail.com>
2019-07-25 11:49:40 +0200

    Added cmake module function to setup conan remotes if they are not available

--- vim/.vim/integrations/templates/cmake/SetupConan.cmake
+++ vim/.vim/integrations/templates/cmake/SetupConan.cmake
@@ -13,3 +13,26 @@ function(get_conan_helper)
   endif()
   include(${CMAKE_BINARY_DIR}/conan.cmake)
 endfunction(get_conan_helper)
+
+function(conan_setup_remotes)
+  # Avoid running this command often
+  if(_conan_has_set_remotes)
+    message(STATUS "Conan remotes already set.")
+    return()
+  endif()
+  # For clara
+  conan_add_remote(NAME
+                   bincrafters
+                   URL
+                   https://api.bintray.com/conan/bincrafters/public-conan)
+
+  # For google benchmark
+  conan_add_remote(NAME
+                   conan-mpusz
+                   URL
+                   https://api.bintray.com/conan/mpusz/conan-mpusz)
+  set(_conan_has_set_remotes
+      TRUE
+      CACHE INTERNAL
+            "This is used to avoid setting the remotes twice. Only takes time.")
+endfunction()
