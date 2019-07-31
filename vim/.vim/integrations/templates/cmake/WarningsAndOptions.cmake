bcd2f365c386ab0afd6c05d69acd28320f9f53e1
srydell <simonwrydell@gmail.com>
2019-07-31 16:35:16 +0200

    Fixed clang not showing absolute paths on errors

--- vim/.vim/integrations/templates/cmake/WarningsAndOptions.cmake
+++ vim/.vim/integrations/templates/cmake/WarningsAndOptions.cmake
@@ -107,6 +107,14 @@ else()
     # filesystem
     target_link_libraries(project_options INTERFACE stdc++fs)
   elseif("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
+    target_compile_options(project_warnings
+                           INTERFACE -Wduplicate-enum
+                                     -fdiagnostics-absolute-paths # To avoid
+                                                                  # confusion
+                                                                  # about which
+                                                                  # file is
+                                                                  # built
+                           )
     # If clang with libc++ no filesystem is required
     if(LIBCPP)
       target_compile_options(project_options INTERFACE -stdlib=libc++)
