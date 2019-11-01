cf568d295bbd65abc1b43df90085cdf5d4829e6c
srydell <simonwrydell@gmail.com>
2019-11-01 16:50:29 +0100

    Added elixir language server updater script

---
+++ bin/bin/update_elixir_ls.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+## Maintainer: Simon Rydell
+## Date created: Nov 01, 2019
+
+cd ~/bin || exit
+
+if [ ! -d ~/bin/elixir-ls ]; then
+	git clone git@github.com:elixir-lsp/elixir-ls.git
+fi
+
+cd ~/bin/elixir-ls || exit
+
+git pull
+
+# Where all the executables will rely
+mkdir release || exit
+
+# Build dependencies
+mix deps.get || exit
+
+# Build the LS
+mix compile || exit
+
+# Put the release build in release
+mix elixir_ls.release -o release
