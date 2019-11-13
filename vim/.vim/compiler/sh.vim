ec916ab8bbb839cebf77d580a0e68b939635c8e7
Simon Rydell <simon.rydell@systecon.se>
2019-11-13 12:51:12 +0100

    Added a shell script to be used with the prototype compiler. Added a new error format for shell scripts and updated the case snippet

--- vim/.vim/compiler/sh.vim
+++ vim/.vim/compiler/sh.vim
@@ -8,9 +8,13 @@ set cpoptions&vim
 
 " Usually use cmake to build out of source
 " use the default 'errorformat'
-CompilerSet errorformat=%f:\ line\ %l:\ %m
+CompilerSet errorformat=
+      \%f:\ line\ %l:\ %m,
+      \%f:\ %l:\ %m
 
 CompilerSet makeprg=if\ [\ \!\ -x\ %:p\ ];\ then\ chmod\ \+x\ %:p;\ fi;\ %:p
 
 let &cpoptions = s:cpo_save
 unlet s:cpo_save
+finish
+" /home/simon/dotfiles/vim/.vim/integrations/compiler/cpp_prototype.sh: 47: /home/simon/dotfiles/vim/.vim/integrations/compiler/cpp_prototype.sh: /home/simon/dotfiles/vim/.vim/build/bin/: Permission denied
---
+++ vim/.vim/integrations/compiler/cpp_prototype.sh
@@ -0,0 +1,113 @@
+#!/bin/sh
+## Maintainer: Simon Rydell
+## Date created: Nov 13, 2019
+
+compiler=""
+flags=""
+path=""
+executable=""
+executable_args=""
+
+common_flags="\
+	-Wall \
+	-Werror \
+	-Wextra \
+	-Wshadow \
+	-Wnon-virtual-dtor \
+	-Wold-style-cast \
+	-Wcast-align \
+	-Wunused \
+	-Woverloaded-virtual \
+	-Wpedantic \
+	-Wconversion \
+	-Wsign-conversion \
+	-Wnull-dereference \
+	-Wdouble-promotion \
+	-Wdate-time \
+	-Wformat=2 \
+	"
+
+clang_flags="\
+	-Wduplicate-enum \
+	-fdiagnostics-absolute-paths \
+	"
+
+gcc_flags="\
+	-Wmisleading-indentation \
+	-Wduplicated-cond \
+	-Wduplicated-branches \
+	-Wlogical-op \
+	-Wuseless-cast \
+	"
+
+# Save in case of error
+input=$*
+
+# Parse input
+while [ -n "$*" ]
+do
+	# Shift once to the option value,
+	# and then once past it
+	case $1 in
+		--compiler )
+			shift
+			case $1 in
+				clang++ )
+					compiler=$(command -v clang++)
+					flags="$common_flags $clang_flags"
+					break
+					;;
+				g++ )
+					compiler=$(command -v g++)
+					flags="$common_flags $gcc_flags"
+					break
+					;;
+				* )
+					echo "Unsupported compiler: $1"
+					exit 1
+					break
+					;;
+			esac
+			shift
+			continue
+			;;
+		--path )
+			shift
+			path="$1"
+			shift
+			continue
+			;;
+		--executable )
+			shift
+			executable="$1"
+			shift
+			continue
+			;;
+		* )
+			# The rest of the arguments
+			# are passed to the executable
+			executable_args="$*"
+			break
+	esac
+done
+
+if [ -z $compiler ] || [ -z $path ] || [ -z $executable ]; then
+	echo "Not enough input specified."
+	echo "Input was:"
+	echo "$input"
+	exit 1
+fi
+
+bin_dir="$PWD/build/bin"
+# Create bin
+if [ ! -d "$bin_dir" ]; then
+	echo mkdir -p "$bin_dir"
+fi
+
+# Compile file to bin
+echo "$compiler" -std=c++17 -O3 -o "$bin_dir/$executable" $path $flags
+
+# If there is an executable, run it
+if [ -n "$executable" ] && [ -x "$bin_dir/$executable" ]; then
+	echo "$bin_dir/$executable" $executable_args
+fi
