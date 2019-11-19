19ca2e36fcd64cc7bd582e6edaf4d277be5a852a
srydell <simonwrydell@gmail.com>
2019-11-19 22:49:31 +0100

    Better defaults for TargetHelpers.cmake

--- vim/.vim/integrations/templates/cmake/TargetHelpers.cmake
+++ vim/.vim/integrations/templates/cmake/TargetHelpers.cmake
@@ -5,14 +5,14 @@ function(create_target)
 
   # Define the supported set of keywords
   set(prefix ARG)
-  set(noValues IS_LIBRARY)
+  set(noValues IS_LIBRARY IS_INTERFACE)
   set(singleValues TARGET)
   set(multiValues
       SOURCES
       INCLUDE
-      PRIVATE_INCLUDE
+      PUBLIC_INCLUDE
       LINK_LIBRARIES
-      PRIVATE_LINK_LIBRARIES)
+      PUBLIC_LINK_LIBRARIES)
 
   # Process the arguments passed in Can be used e.g. via ARG_TARGET
   cmake_parse_arguments(${prefix}
@@ -24,29 +24,34 @@ function(create_target)
   if(NOT ARG_TARGET)
     message(FATAL_ERROR "Must provide a target.")
   endif()
+
   if(NOT ARG_SOURCES)
-    message(FATAL_ERROR "Must provide sources.")
+    if(NOT ARG_IS_INTERFACE)
+      message(FATAL_ERROR "Must provide sources.")
+    endif()
   endif()
 
   if(ARG_IS_LIBRARY)
     add_library(${ARG_TARGET} ${ARG_SOURCES})
+  elseif(ARG_IS_INTERFACE)
+    add_library(${ARG_TARGET} INTERFACE)
   else()
     add_executable(${ARG_TARGET} ${ARG_SOURCES})
   endif()
 
-  if(ARG_INCLUDE)
+  if(ARG_PUBLIC_INCLUDE)
     target_include_directories(${ARG_TARGET} PUBLIC ${ARG_INCLUDE})
   endif()
 
-  if(ARG_PRIVATE_INCLUDE)
+  if(ARG_INCLUDE)
     target_include_directories(${ARG_TARGET} PRIVATE ${ARG_INCLUDE})
   endif()
 
-  if(ARG_LINK_LIBRARIES)
-    target_link_libraries(${ARG_TARGET} PUBLIC ${ARG_LINK_LIBRARIES})
+  if(ARG_PUBLIC_LINK_LIBRARIES)
+    target_link_libraries(${ARG_TARGET} PUBLIC ${ARG_PUBLIC_LINK_LIBRARIES})
   endif()
 
-  if(ARG_PRIVATE_LINK_LIBRARIES)
+  if(ARG_LINK_LIBRARIES)
     target_link_libraries(${ARG_TARGET} PRIVATE ${ARG_LINK_LIBRARIES})
   endif()
 
