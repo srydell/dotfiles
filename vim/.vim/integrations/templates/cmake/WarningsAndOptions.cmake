39141345ad314b2831c8cfe022515e51c7adb382
Simon Rydell <simon.rydell@systecon.se>
2019-12-12 11:01:05 +0100

    Add description to MSVC flags

--- vim/.vim/integrations/templates/cmake/WarningsAndOptions.cmake
+++ vim/.vim/integrations/templates/cmake/WarningsAndOptions.cmake
@@ -10,29 +10,31 @@ add_library(project_warnings INTERFACE)
 
 # Set warning flags for different compilers
 if("${CMAKE_CXX_COMPILER_ID}" MATCHES "MSVC")
-  target_compile_options(project_warnings
-                         INTERFACE /W4
-                                   /WX
-                                   "/permissive-"
-                                   /w14640
-                                   /w14242
-                                   /w14254
-                                   /w14263
-                                   /w14265
-                                   /w14287
-                                   /we4289
-                                   /w14296
-                                   /w14311
-                                   /w14545
-                                   /w14546
-                                   /w14547
-                                   /w14549
-                                   /w14555
-                                   /w14619
-                                   /w14826
-                                   /w14905
-                                   /w14906
-                                   /w14928)
+  target_compile_options(
+    project_warnings
+    INTERFACE
+      /W4
+      /WX
+      "/permissive-"
+      /w14242 # 'identfier': conversion from 'type1' to 'type1', possible loss of data¬
+      /w14254 # 'operator': conversion from 'type1:field_bits' to 'type2:field_bits', possible loss of data¬
+      /w14263 # 'function': member function does not override any base class virtual member function¬
+      /w14265 # 'classname': class has virtual functions, but destructor is not virtual instances of this class may not be destructed correctly¬
+      /w14287 # 'operator': unsigned/negative constant mismatch¬
+      /we4289 # nonstandard extension used: 'variable': loop control variable declared in the for-loop is used outside the for-loop scope¬
+      /w14296 # 'operator': expression is always 'boolean_value'¬
+      /w14311 # 'variable': pointer truncation from 'type1' to 'type2'¬
+      /w14545 # expression before comma evaluates to a function which is missing an argument list¬
+      /w14546 # function call before comma missing argument list¬
+      /w14547 # 'operator': operator before comma has no effect; expected operator with side-effect ¬
+      /w14549 # 'operator': operator before comma has no effect; did you intend 'operator'?¬
+      /w14555 # expression has no effect; expected expression with side-effect¬
+      /w14619 # pragma warning: there is no warning number 'number'¬
+      /w14826 # Conversion from 'type1' to 'type_2' is sign-extended. This may cause unexpected runtime behavior.¬
+      /w14905 # wide string literal cast to 'LPSTR'¬
+      /w14906 # string literal cast to 'LPWSTR'¬
+      /w14928 # illegal copy-initialization; more than one user-defined conversion has been implicitly applied
+      /w14640)
 else()
   # Either gcc or Clang
 
