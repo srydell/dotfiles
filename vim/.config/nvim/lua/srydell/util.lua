2e8029bd8babd2f052cc19dd96ff60508231ace0
srydell <simonwrydell@gmail.com>
2023-12-03 17:29:45 +0100

    Xcodebuild config

--- vim/.config/nvim/lua/srydell/util.lua
+++ vim/.config/nvim/lua/srydell/util.lua
@@ -141,4 +141,42 @@ M.merge = function(l0, l1)
   return l
 end
 
+local cachedConfig = {}
+local searchedForConfig = {}
+
+-- Find config file
+-- Caches each run
+function M.find_config(filename)
+  if searchedForConfig[filename] then
+    return cachedConfig[filename]
+  end
+
+  local configs = vim.fn.systemlist({
+    'find',
+    vim.fn.getcwd(),
+    '-maxdepth',
+    '2',
+    '-iname',
+    filename,
+    '-not',
+    '-path',
+    '*/.*/*',
+  })
+  searchedForConfig[filename] = true
+
+  if vim.v.shell_error ~= 0 then
+    return nil
+  end
+
+  table.sort(configs, function(a, b)
+    return a ~= '' and #a < #b
+  end)
+
+  if configs[1] then
+    cachedConfig[filename] = string.match(configs[1], '^%s*(.-)%s*$')
+  end
+
+  return cachedConfig[filename]
+end
+
 return M
