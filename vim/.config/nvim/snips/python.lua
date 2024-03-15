572e1723fc80cba29b699df79ef52318e1f840ac
Simon Rydell <simon.rydell@nasdaq.com>
2024-03-15 15:14:25 +0100

    Add support for copilot

--- vim/.config/nvim/snips/python.lua
+++ vim/.config/nvim/snips/python.lua
@@ -2,36 +2,94 @@ local helpers = require('srydell.snips.helpers')
 local get_visual = helpers.get_visual
 
 return {
-  s({ trig='^(%s*)i', regTrig=true, dscr='import statement' },
-    {
-      c(1, {
-          sn(nil, { t('import '), r(1, 'package') }),
-          sn(nil, { t('from '), r(1, 'package'), t(' import '), i(2) }),
-        }),
-    },
-    {
-      stored = {
-        -- key passed to restoreNodes.
-        ['package'] = i(1, 'statistics')
-      }
-    }
-  ),
-
-  s({ trig='f', dscr='Simple function' },
+  s(
+    { trig = 'str', wordTrig = true, dscr = 'data class' },
     fmta(
-        [[
-          def <>(<>):
-              <>
-        ]],
-        {
-          i(1, 'f'),
-          i(2),
-          i(0),
-        }
-      )
+      [[
+        @dataclass
+        class <>:
+            <>
+      ]],
+      {
+        i(1, 'Data'),
+        i(0),
+      }
+    )
   ),
 
-  s({ trig='if', wordTrig=true, dscr='if statement' },
+  s(
+    { trig = 'cls', wordTrig = true, dscr = 'Class' },
+    fmta(
+      [[
+        class <>:
+            <>
+      ]],
+      {
+        i(1, 'ClassName'),
+        i(0),
+      }
+    )
+  ),
+
+  s(
+    { trig = 'for', wordTrig = true, dscr = 'for loop' },
+    fmta(
+      [[
+        for <> in <>:
+            <>
+      ]],
+      {
+        i(1, 'thing'),
+        i(2, 'things'),
+        i(0),
+      }
+    )
+  ),
+
+  s(
+    { trig = 'with', wordTrig = true, dscr = 'with open' },
+    fmta(
+      [[
+        with open("<>") as <>:
+            <>
+      ]],
+      {
+        i(1, 'filename.txt'),
+        i(2, 'f'),
+        i(0),
+      }
+    )
+  ),
+
+  s({ trig = '^(%s*)i', regTrig = true, dscr = 'import statement' }, {
+    c(1, {
+      sn(nil, { t('import '), r(1, 'package') }),
+      sn(nil, { t('from '), r(1, 'package'), t(' import '), i(2) }),
+    }),
+  }, {
+    stored = {
+      -- key passed to restoreNodes.
+      ['package'] = i(1, 'statistics'),
+    },
+  }),
+
+  s(
+    { trig = 'f', dscr = 'Simple function' },
+    fmta(
+      [[
+        def <>(<>):
+            <>
+      ]],
+      {
+        i(1, 'f'),
+        i(2),
+        i(0),
+      }
+    )
+  ),
+
+  s(
+    { trig = 'if', wordTrig = true, dscr = 'if statement' },
     fmta(
       [[
         if <>:
@@ -39,14 +97,24 @@ return {
       ]],
       {
         c(1, {
-          sn(nil, fmta(
-            [[
-              <>
-            ]], { i(1) })),
-          sn(nil, fmta(
-            [[
-              re.match(<>, <>)
-            ]], { i(1, 'pattern'), i(2, 'input') })),
+          sn(
+            nil,
+            fmta(
+              [[
+                <>
+              ]],
+              { i(1) }
+            )
+          ),
+          sn(
+            nil,
+            fmta(
+              [[
+                re.match(<>, <>)
+              ]],
+              { i(1, 'pattern'), i(2, 'input') }
+            )
+          ),
         }),
         d(2, get_visual),
         i(0),
@@ -54,22 +122,23 @@ return {
     )
   ),
 
-  s({ trig='p', wordTrig=true, dscr='print statement' },
-      fmta(
-        [[
-          print(f"<>")
-        ]],
-        { i(1) }
-      )
+  s(
+    { trig = 'p', wordTrig = true, dscr = 'print statement' },
+    fmta(
+      [[
+        print(<>)
+      ]],
+      { i(1) }
+    )
   ),
 
-  s({ trig='pv', wordTrig=true, dscr='print variable' },
-      fmta(
-        [[
-          print(f"<> = {<>}")
-        ]],
-        { rep(1), i(1) }
-      )
+  s(
+    { trig = 'pv', wordTrig = true, dscr = 'print variable' },
+    fmta(
+      [[
+        print(f"<> = {<>}")
+      ]],
+      { rep(1), i(1) }
+    )
   ),
-
 }
