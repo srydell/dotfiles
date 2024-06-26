434433c1c2230374678ffaf02c912b960f9fbb12
Simon Rydell <simon.rydell@nasdaq.com>
2024-06-26 13:21:29 +0200

    Update jdtls

--- vim/.config/nvim/snips/markdown.lua
+++ vim/.config/nvim/snips/markdown.lua
@@ -6,43 +6,27 @@ local repeat_capture = function(_, snip)
 end
 
 return {
-  postfix({ trig='.c', dscr='Inline code' },
-    { l('`' .. l.POSTFIX_MATCH .. '`'), }
-  ),
+  postfix({ trig = '.c', dscr = 'Inline code' }, { l('`' .. l.POSTFIX_MATCH .. '`') }),
 
-  postfix({ trig='.i', dscr='Italic text' },
-    { l('*' .. l.POSTFIX_MATCH .. '*'), }
-  ),
+  postfix({ trig = '.i', dscr = 'Italic text' }, { l('*' .. l.POSTFIX_MATCH .. '*') }),
 
-  postfix({ trig='.b', dscr='Bold text' },
-    { l('**' .. l.POSTFIX_MATCH .. '**'), }
-  ),
+  postfix({ trig = '.b', dscr = 'Bold text' }, { l('**' .. l.POSTFIX_MATCH .. '**') }),
 
-  s({ trig='link', wordTrig=true, dscr='A link' },
-    {
-      c(1, {
-          sn(nil, fmta(
-              [[
-                [<>](<>)
-              ]], { r(1, 'link text'), r(2, 'link') })),
-            sn(nil, fmta(
-              [[
-                [<>](<> "<>")
-              ]], { r(1, 'link text'), r(2, 'link'), i(3, 'On hover text') })),
-        }
-      ),
-    },
-    {
-      stored = {
-          -- key passed to restoreNodes.
-          ['link text'] = i(1),
-          ['link'] = i(2)
+  s(
+    { trig = 'link', wordTrig = true, dscr = 'A link' },
+    fmta(
+      [[
+        [<>](<>)
+      ]],
+      {
+        i(1, 'link text'),
+        i(2, 'link'),
       }
-    }
+    )
   ),
 
-
-  s({ trig='img', wordTrig=true, dscr='Image' },
+  s(
+    { trig = 'img', wordTrig = true, dscr = 'Image' },
     fmta(
       [[
         ![<>](<>)
@@ -54,16 +38,20 @@ return {
     )
   ),
 
-  s({ trig='cbl', wordTrig=true, dscr='Code block' },
+  s(
+    { trig = 'cbl', wordTrig = true, dscr = 'Code block' },
     fmta(
       [[
         ```<>
         <>
         ```
-      ]], { i(1, 'sh'), d(2, get_visual) })
-    ),
+      ]],
+      { i(1, 'sh'), d(2, get_visual) }
+    )
+  ),
 
-  s({trig = '(s+)ec', regTrig = true, dscr = "Section based on how many 's'"},
+  s(
+    { trig = '(s+)ec', regTrig = true, dscr = "Section based on how many 's'" },
     fmt(
       [[
           {} {}
@@ -75,5 +63,4 @@ return {
       }
     )
   ),
-
 }
