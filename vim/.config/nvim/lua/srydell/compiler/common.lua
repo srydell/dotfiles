6beb6f1f5d6ce3b537fb9f017f9b6c4474c3b121
Simon Rydell <simon.rydell@nasdaq.com>
2024-07-04 08:50:40 +0200

    Support for window based compilers

--- vim/.config/nvim/lua/srydell/compiler/common.lua
+++ vim/.config/nvim/lua/srydell/compiler/common.lua
@@ -21,16 +21,26 @@ M.run = function()
   task:start()
 end
 
--- Internal function to make sure vim.bo.srydell_current_compiler has a value
+-- Internal function to make sure vim.w.srydell_current_compiler has a value
 -- and to get the compilers related to the current filetype
 local function initiate_compilers()
-  local status, compilers = pcall(require, 'srydell.compiler.filetype.' .. vim.bo.ft)
+  -- Fetch the list of compilers for this filetype
+  local status, compilers = pcall(require, 'srydell.compiler.filetype.' .. vim.bo.filetype)
   if not status then
     return nil
   end
 
-  if vim.b.srydell_current_compiler == nil then
-    vim.b.srydell_current_compiler = 1
+  -- Make sure that the compiler index exists for this filetype
+  if vim.w.srydell_current_compiler == nil then
+    -- First time opening vim
+    local c = {}
+    c[vim.bo.filetype] = 1
+    vim.w.srydell_current_compiler = c
+  elseif vim.w.srydell_current_compiler[vim.bo.filetype] == nil then
+    -- Opening a new filetype
+    local c = vim.w.srydell_current_compiler
+    c[vim.bo.filetype] = 1
+    vim.w.srydell_current_compiler = c
   end
 
   return compilers
@@ -42,12 +52,36 @@ M.get_current_compiler = function()
     return nil
   end
 
-  if #compilers < vim.b.srydell_current_compiler then
+  local index = vim.w.srydell_current_compiler[vim.bo.filetype]
+  if #compilers < index then
     -- No compilers
     return nil
   end
 
-  return compilers[vim.b.srydell_current_compiler]
+  return compilers[index]
+end
+
+-- Moves the compiler index +1 or -1 depending on direction
+-- number_of_compilers is for bounds checking
+local function change_compiler(direction, number_of_compilers)
+  local index = vim.w.srydell_current_compiler[vim.bo.filetype]
+  if direction == 1 then
+    if index == number_of_compilers then
+      index = 1
+    elseif index < number_of_compilers then
+      index = index + 1
+    end
+  elseif direction == -1 then
+    if index == 1 then
+      index = number_of_compilers
+    elseif index > 1 then
+      index = index - 1
+    end
+  end
+
+  local new_compilers = vim.w.srydell_current_compiler
+  new_compilers[vim.bo.filetype] = index
+  vim.w.srydell_current_compiler = new_compilers
 end
 
 M.go_to_next_compiler = function()
@@ -56,14 +90,7 @@ M.go_to_next_compiler = function()
     return nil
   end
 
-  local next = vim.b.srydell_current_compiler
-  if next == #compilers then
-    next = 1
-  elseif next < #compilers then
-    next = next + 1
-  end
-
-  vim.b.srydell_current_compiler = next
+  change_compiler(1, #compilers)
 end
 
 M.go_to_previous_compiler = function()
@@ -72,14 +99,7 @@ M.go_to_previous_compiler = function()
     return nil
   end
 
-  local previous = vim.b.srydell_current_compiler
-  if previous == 1 then
-    previous = #compilers
-  elseif previous > 1 then
-    previous = previous - 1
-  end
-
-  vim.b.srydell_current_compiler = previous
+  change_compiler(-1, #compilers)
 end
 
 return M
--- vim/.config/nvim/lua/srydell/compiler/filetype/lua.lua
+++ vim/.config/nvim/lua/srydell/compiler/filetype/lua.lua
@@ -1,14 +1,5 @@
-local function my_test()
-  vim.print('Hello from inside the test')
-  return true
-end
-
 local function get_compilers()
   return {
-    {
-      name = 'func test',
-      tasks = { { 'lua function', f = my_test }, { 'shell', cmd = 'echo hello from later' } },
-    },
     { name = 'lua run', tasks = { 'lua' } },
   }
 end
