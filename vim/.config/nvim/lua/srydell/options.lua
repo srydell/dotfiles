1dc4a6115445663bc5488ca62603231776a08451
srydell <simon.rydell@nasdaq.com>
2025-04-17 09:29:38 +0200

    Avoid using for loop

--- vim/.config/nvim/lua/srydell/options.lua
+++ vim/.config/nvim/lua/srydell/options.lua
@@ -1,143 +1,139 @@
 local home = os.getenv('HOME')
-for option, value in pairs({
-  encoding = 'utf-8',
+vim.opt.encoding = 'utf-8'
 
-  -- Make it easier to see tabs, newlines, and trailing spaces
-  list = true,
-  listchars = { tab = '▸ ', eol = '¬', trail = '·' },
+-- Make it easier to see tabs, newlines, and trailing spaces
+vim.opt.list = true
+vim.opt.listchars = { tab = '▸ ', eol = '¬', trail = '·' }
 
-  -- BOX DRAWINGS HEAVY VERTICAL (U+2503, UTF-8: E2 94 83)
-  -- MIDDLE DOT (U+00B7, UTF-8: C2 B7)
-  -- Draw the vertical border between vim splits as a continuous line
-  -- Draw the character used to fill out the fold
-  fillchars = { vert = '┃', fold = '·' },
+-- BOX DRAWINGS HEAVY VERTICAL (U+2503, UTF-8: E2 94 83)
+-- MIDDLE DOT (U+00B7, UTF-8: C2 B7)
+-- Draw the vertical border between vim splits as a continuous line
+-- Draw the character used to fill out the fold
+vim.opt.fillchars = { vert = '┃', fold = '·' }
 
-  -- Not as cool as syntax, but faster
-  foldmethod = 'indent',
+-- Not as cool as syntax, but faster
+vim.opt.foldmethod = 'indent'
 
-  -- Start unfolded
-  foldlevelstart = 99,
-  -- foldtext = folding#FoldText()
+-- Start unfolded
+vim.opt.foldlevelstart = 99
+-- vim.opt.foldtext = folding#FoldText()
 
-  -- Show absolute rows
-  number = true,
-  relativenumber = false,
+-- Show absolute rows
+vim.opt.number = true
+vim.opt.relativenumber = false
 
-  -- Make backspace be able to delete indent and before starting position
-  backspace = { 'indent', 'eol', 'start' },
+-- Make backspace be able to delete indent and before starting position
+vim.opt.backspace = { 'indent', 'eol', 'start' }
 
-  -- Keep the cursor in the middle of the screen
-  scrolloff = 999,
+-- Keep the cursor in the middle of the screen
+vim.opt.scrolloff = 999
 
-  -- Cursor visuals.     Block                  ,  Underscore
-  guicursor = { 'n-v-c-sm-i-ci-ve:block', 'r-cr-o:hor20' },
+-- Cursor visuals.     Block                  ,  Underscore
+vim.opt.guicursor = { 'n-v-c-sm-i-ci-ve:block', 'r-cr-o:hor20' }
 
-  -- Show commands as they are being written
-  showcmd = true,
+-- Show commands as they are being written
+vim.opt.showcmd = true
 
-  -- Do not show the current mode (insert, visual, ...)
-  showmode = false,
+-- Do not show the current mode (insert, visual, ...)
+vim.opt.showmode = false
 
-  -- Softwrap text (without creating a newline)
-  wrap = true,
+-- Softwrap text (without creating a newline)
+vim.opt.wrap = true
 
-  -- Make vim highlight search while typing
-  incsearch = true,
+-- Make vim highlight search while typing
+vim.opt.incsearch = true
 
-  -- Auto read file when changed while open
-  autoread = true,
+-- Auto read file when changed while open
+vim.opt.autoread = true
 
-  -- Auto write on :make
-  autowrite = true,
+-- Auto write on :make
+vim.opt.autowrite = true
 
-  -- Make vim exit visual mode without delay
-  timeoutlen = 1000,
-  ttimeoutlen = 0,
+-- Make vim exit visual mode without delay
+vim.opt.timeoutlen = 1000
+vim.opt.ttimeoutlen = 0
 
-  -- Don't redraw buffer while evaluating macros
-  lazyredraw = true,
+-- Don't redraw buffer while evaluating macros
+vim.opt.lazyredraw = true
 
-  -- Be able to hide unsaved buffers while editing new ones
-  hidden = true,
+-- Be able to hide unsaved buffers while editing new ones
+vim.opt.hidden = true
 
-  -- Ignore case if search is lowercase, otherwise case-sensitive
-  ignorecase = true,
-  smartcase = true,
+-- Ignore case if search is lowercase, otherwise case-sensitive
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
-  -- Allow the visual block to not be restricted by EOL
-  virtualedit = 'block',
+-- Allow the visual block to not be restricted by EOL
+vim.opt.virtualedit = 'block'
 
-  -- Disable error feedback via flashing screen
-  visualbell = false,
+-- Disable error feedback via flashing screen
+vim.opt.visualbell = false
 
-  -- Default to splitting below and to the right with :split :vsplit
-  splitbelow = true,
-  splitright = true,
+-- Default to splitting below and to the right with :split :vsplit
+vim.opt.splitbelow = true
+vim.opt.splitright = true
 
-  -- Better autocomplete
-  completeopt = { 'longest', 'menuone', 'noselect', 'preview' },
+-- Better autocomplete
+vim.opt.completeopt = { 'longest', 'menuone', 'noselect', 'preview' }
 
-  -- Maximum number of autocomplete items
-  pumheight = 6,
-  previewheight = 6,
+-- Maximum number of autocomplete items
+vim.opt.pumheight = 6
+vim.opt.previewheight = 6
 
-  -- Completion with commands
-  wildmenu = true,
-  wildmode = { 'longest', 'full' },
+-- Completion with commands
+vim.opt.wildmenu = true
+vim.opt.wildmode = { 'longest', 'full' }
 
-  -- Ignore the following
-  wildignore = {
-    '*.o',
-    '*.a',
-    '*.aux',
-    '*.out',
-    '*.toc',
-    '__pycache__',
-    '.git',
-    'build',
-    '*.sw?',
-    '*.DS_Store',
-    '*.pyc',
-    '*.jpg',
-    '*.bmp',
-    '*.jpeg',
-    '*.gif',
-    '*.png',
-  },
+-- Ignore the following
+vim.opt.wildignore = {
+  '*.o',
+  '*.a',
+  '*.aux',
+  '*.out',
+  '*.toc',
+  '__pycache__',
+  '.git',
+  'build',
+  '*.sw?',
+  '*.DS_Store',
+  '*.pyc',
+  '*.jpg',
+  '*.bmp',
+  '*.jpeg',
+  '*.gif',
+  '*.png',
+}
 
-  -- Let vim store backup/swap/undo files in these directories
-  -- The double // will create files with whole path expanded.
-  backupdir = home .. '/.config/nvim/tmp/backup//',
-  directory = home .. '/.config/nvim/tmp/swap//',
-  undodir = home .. '/.config/nvim/tmp/undo//',
+-- Let vim store backup/swap/undo files in these directories
+-- The double // will create files with whole path expanded.
+vim.opt.backupdir = home .. '/.config/nvim/tmp/backup//'
+vim.opt.directory = home .. '/.config/nvim/tmp/swap//'
+vim.opt.undodir = home .. '/.config/nvim/tmp/undo//'
 
-  -- Delete old backup, backup current file
-  backup = true,
-  writebackup = true,
+-- Delete old backup, backup current file
+vim.opt.backup = true
+vim.opt.writebackup = true
 
-  -- Persistent undo tree after exiting vim
-  undofile = true,
+-- Persistent undo tree after exiting vim
+vim.opt.undofile = true
 
-  -- How many levels are saved in each file
-  undolevels = 100,
+-- How many levels are saved in each file
+vim.opt.undolevels = 100
 
-  -- Better display for messages
-  cmdheight = 2,
+-- Better display for messages
+vim.opt.cmdheight = 2
 
-  -- For faster diagnostics
-  updatetime = 300,
+-- For faster diagnostics
+vim.opt.updatetime = 300
 
-  -- Always show signcolumns
-  signcolumn = 'yes',
+-- Always show signcolumns
+vim.opt.signcolumn = 'yes'
 
-  -- Use sh. It's faster than bash
-  shell = 'sh',
+-- Use sh. It's faster than bash
+vim.opt.shell = 'sh'
 
-  -- Do not highlight searched terms
-  hlsearch = false,
-}) do
-  vim.opt[option] = value
-end
+-- Do not highlight searched terms
+vim.opt.hlsearch = false
 
 -- Don't give |ins-completion-menu| messages.
 vim.opt.shortmess:append('c')
