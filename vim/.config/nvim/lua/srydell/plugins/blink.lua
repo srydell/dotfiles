e5a666247a5d1c480639fc48f70368679664c07f
srydell <simon.rydell@nasdaq.com>
2025-05-30 09:04:21 +0200

    still trying blink

--- vim/.config/nvim/lua/srydell/plugins/blink.lua
+++ vim/.config/nvim/lua/srydell/plugins/blink.lua
@@ -7,79 +7,61 @@ return {
 
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
-  opts = {
-    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-    -- 'super-tab' for mappings similar to vscode (tab to accept)
-    -- 'enter' for enter to accept
-    -- 'none' for no mappings
-    --
-    -- All presets have the following mappings:
-    -- C-space: Open menu or open docs if already open
-    -- C-n/C-p or Up/Down: Select next/previous item
-    -- C-e: Hide menu
-    -- C-k: Toggle signature help (if signature.enabled = true)
-    --
-    -- See :h blink-cmp-config-keymap for defining your own keymap
-    -- in your blink configuration
-    keymap = {
-      preset = 'default',
-      ['<C-e>'] = { 'snippet_forward', 'fallback' },
-      ['<C-h>'] = { 'snippet_backward', 'fallback' },
-      -- ['<C-E>'] = cmp.mapping(function(fallback)
+  config = function()
+    local ls = require('luasnip')
+    local blink = require('blink.cmp')
+    blink.setup({
+      keymap = {
+        preset = 'default',
+        ['<Tab>'] = { 'snippet_forward', 'fallback' },
+        ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+
+        ['<Up>'] = { 'select_prev', 'fallback' },
+        ['<Down>'] = { 'select_next', 'fallback' },
+        ['<C-n>'] = { 'select_next', 'fallback' },
+        ['<C-p>'] = { 'select_prev', 'fallback' },
+
+        ['<S-k>'] = { 'scroll_documentation_up', 'fallback' },
+        ['<S-j>'] = { 'scroll_documentation_down', 'fallback' },
+
+        ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+        ['<C-e>'] = { 'hide', 'fallback' },
+      },
+
+      snippets = { preset = 'luasnip' },
+
+      appearance = {
+        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = 'mono',
+      },
+
+      -- (Default) Only show the documentation popup when manually triggered
+      completion = {
+        list = {
+          selection = {
+            preselect = function(ctx)
+              return not require('blink.cmp').snippet_active({ direction = 1 })
+            end,
+            auto_insert = true,
+          },
+        },
+        documentation = { auto_show = false },
+      },
+
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+      },
+
+      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
       --
-      --   if ls.expand_or_jumpable() then
-      --     ls.expand_or_jump()
-      --   else
-      --     fallback()
-      --   end
-      -- end, { 'i', 's' }),
-      -- ['<C-H>'] = cmp.mapping(function(fallback)
-      --   if ls.jumpable(-1) then
-      --     ls.jump(-1)
-      --   else
-      --     fallback()
-      --   end
-      -- end, { 'i', 's' }),
-      -- ['<C-e>'] = {
-      --   function(cmp)
-      --     if cmp.snippet_active() then
-      --       return cmp.accept()
-      --     else
-      --       return cmp.select_and_accept()
-      --     end
-      --   end,
-      --   'snippet_forward',
-      --   'fallback',
-      -- },
-      -- ['<C-h>'] = { 'snippet_backward', 'fallback' },
-    },
-
-    snippets = { preset = 'luasnip' },
-
-    appearance = {
-      -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- Adjusts spacing to ensure icons are aligned
-      nerd_font_variant = 'mono',
-    },
-
-    -- (Default) Only show the documentation popup when manually triggered
-    completion = {
-      list = { selection = { preselect = false, auto_insert = true } },
-      documentation = { auto_show = false },
-    },
-
-    -- Default list of enabled providers defined so that you can extend it
-    -- elsewhere in your config, without redefining it, due to `opts_extend`
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-    },
-
-    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-    --
-    -- See the fuzzy documentation for more information
-    fuzzy = { implementation = 'prefer_rust_with_warning' },
-  },
+      -- See the fuzzy documentation for more information
+      fuzzy = { implementation = 'prefer_rust_with_warning' },
+    })
+  end,
   opts_extend = { 'sources.default' },
 }
