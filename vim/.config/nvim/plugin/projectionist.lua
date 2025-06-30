e1e98b52004fa55684a2744b5b7ce69eeeec30ff
srydell <simon.rydell@nasdaq.com>
2025-06-30 09:19:47 +0200

    Some project specific projectionist settings

--- vim/.config/nvim/plugin/projectionist.lua
+++ vim/.config/nvim/plugin/projectionist.lua
@@ -23,5 +23,15 @@ vim.g.projectionist_heuristics = {
       alternate = { 'src/{}.cpp', 'src/{}.hpp' },
       type = 'source',
     },
+    ['test/test_*.cpp'] = {
+      alternate = { 'src/{}.cpp', 'src/{}.hpp' },
+      type = 'source',
+    },
+    ['src/test/func/test_scenarios/scenario_*.txt'] = {
+      alternate = { 'build_artifacts/log_ScenarioTests_{}.txt' },
+    },
+    ['build_artifacts/log_ScenarioTests_*.txt'] = {
+      alternate = { 'src/test/func/test_scenarios/scenario_{}.txt' },
+    },
   },
 }
