23a2d3c5a3354909cb39a36825e08a11b5db0be8
srydell <simon.rydell@nasdaq.com>
2026-08-20 09:39:46 +0200

    Hook up StructLayout

--- vim/.config/nvim/ftplugin/cpp.lua
+++ vim/.config/nvim/ftplugin/cpp.lua
@@ -12,3 +12,10 @@ vim.keymap.set('n', '<leader>rcc', ts_cpp.make_class_constructor, { buffer = tru
 vim.keymap.set('n', '<leader>rcd', ts_cpp.make_class_destructor, { buffer = true })
 vim.keymap.set('n', '<leader>rcnm', ts_cpp.make_class_no_move, { buffer = true })
 vim.keymap.set('n', '<leader>rcnc', ts_cpp.make_class_no_copy, { buffer = true })
+
+-- Show the memory layout (offsets/padding/size/align) of the struct/class
+-- under the cursor. See lua/srydell/util/struct_layout.lua.
+vim.api.nvim_buf_create_user_command(0, 'StructLayout', function()
+  require('srydell.util.struct_layout').show()
+end, {})
+vim.keymap.set('n', '<leader>rsl', '<cmd>StructLayout<CR>', { buffer = true })
